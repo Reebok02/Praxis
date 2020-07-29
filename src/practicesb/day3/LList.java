5106b7f98027fa63226b2b11ccea2da0a18edf7f
Reebok02 <51023762+Reebok02@users.noreply.github.com>
2020-07-29 17:06:59 +0300

    LList, get() and contains()

---
+++ src/practicesb/day3/LList.java
@@ -0,0 +1,103 @@
+package practicesb.day3;
+
+import java.util.LinkedList;
+import java.util.List;
+
+public class LList {
+    private Node root;
+
+    public void add(Object obj) {
+        if (obj == null) {
+            return;
+        }
+        Node node = new Node(obj);
+        Node last = getLast();
+        if (last == null) {
+            root = node;
+        } else {
+            last.next = node;
+        }
+    }
+    public boolean contains(Object obj){
+        if (obj == null || this.size() ==0){
+            return false;
+        } else {
+            for (int i = 0; i < this.size(); i++){
+                if (obj.equals(this.get(i))){
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+
+    public int size(){
+        if (root == null){
+            return 0;
+        } else {
+            int count = 1;
+            Node currentNode = root;
+            while (currentNode.next != null){
+                currentNode = currentNode.next;
+                count++;
+            }
+            return count;
+        }
+    }
+
+    public Object get (int index){
+        if (index < 0 || index + 1 > size()) {
+            return null;
+        } else {
+            Node currentNode = root;
+            int i = 0;
+            while (index != i){
+                currentNode = currentNode.next;
+                i++;
+            }
+            return currentNode.data;
+        }
+
+    }
+
+    private Node getLast() {
+        if (root == null) {
+            return null;
+        }
+        Node currentNode = root;
+        while (currentNode.next != null) {
+            currentNode = currentNode.next;
+        }
+        return currentNode;
+    }
+
+    private class Node {
+        Object data;
+        Node next;
+
+        public Node(Object item) {
+            this.data = item;
+        }
+    }
+}
+class MainTaskLList {
+    public static void main(String[] args) {
+        List list1 = new LinkedList();
+        LList list = new LList();
+        list.add("String 1");
+        list.add("String 2");
+        list.add(123);
+        list.add("String 4");
+        list.add("String 5");
+        list.add("String 6");
+        list.add("String 7");
+        System.out.println(list.get(100));
+        System.out.println(list.get(2));
+        System.out.println(list.size());
+        System.out.println(list.contains(null));
+        System.out.println(list.contains(123));
+        System.out.println(list.contains("String 5"));
+        System.out.println(list.size());
+    }
+}
+
