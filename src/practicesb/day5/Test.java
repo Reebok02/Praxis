9d72d5a021431608ce8cec60e2eedeff87f7cd3c
Reebok02 <51023762+Reebok02@users.noreply.github.com>
2020-07-31 11:17:50 +0300

    ready

--- src/practicesb/day5/Test.java
+++ src/practicesb/day5/Test.java
@@ -1,4 +1,98 @@
 package practicesb.day5;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 public class Test {
+    public static void main(String[] args) {
+        List<String> group1 = new ArrayList<>(Arrays.asList("Анастасия", "Ксения", "Юрий"));
+        List<String> group2 = new ArrayList<>(Arrays.asList("Антон", "Ксения", "Юлия"));
+        List<Integer> numbers1 = new ArrayList<>(Arrays.asList(1, 2, 3));
+        List<Double> numbers2 = new ArrayList<>(Arrays.asList(1.5, 2.5, 3.5));
+        List<String> nullList = null;
+        List<String> emptyList = new ArrayList<>();
+
+        boolean isBlank;
+
+        isBlank = UtilClass.isBlank(nullList);
+
+        if (!isBlank) {
+            throw new AssertionError("Должно быть значение true");
+        }
+
+        isBlank = UtilClass.isBlank(emptyList);
+
+        if (!isBlank) {
+            throw new AssertionError("Должно быть значение true");
+        }
+
+        isBlank = UtilClass.isBlank(group1);
+
+        if (isBlank) {
+            throw new AssertionError("Должно быть значение false");
+        }
+
+        String lastElem = (String) UtilClass.getLastElem(group1);
+
+        if (!"Юрий".equals(lastElem)) {
+            throw new AssertionError("Последний в списке должен быть Юрий");
+        }
+
+        double lastNum = (double) UtilClass.getLastElem(numbers1);
+
+        if (lastNum != 3) {
+            throw new AssertionError("Последним в списке должен быть 3");
+        }
+
+        String name = (String) UtilClass.findElem(group1, "Ксения");
+
+        if (!"Ксения".equals(name)) {
+            throw new AssertionError("Найденным должна быть Ксения");
+        }
+
+        int num = (int) UtilClass.findElem(numbers1, 2);
+
+        if (num != 2) {
+            throw new AssertionError("Найденным должна быть 2");
+        }
+
+        String nullName = (String) UtilClass.findElem(group1, "Кирилл");
+
+        if (nullName != null) {
+            throw new AssertionError("Кирилла в списке нет, должен вернуться null");
+        }
+
+        List<String> union = UtilClass.union(group1, group2);
+
+        if (group1.size() != 3 || group2.size() != 3) {
+            throw new AssertionError("Изначальные списки не должны измениться");
+        }
+
+        if (union.size() != 6
+                || !union.containsAll(Arrays.asList("Анастасия", "Ксения", "Юрий", "Антон", "Ксения", "Юлия"))
+        ) {
+            throw new AssertionError("ответ union содержит неправильный список имен");
+        }
+
+        List<String> cutGroup = UtilClass.removeAll(group1, group2);
+
+        if (group1.size() != 3 || group2.size() != 3) {
+            throw new AssertionError("Изначальные списки не должны измениться");
+        }
+
+        if (cutGroup.size() != 2
+                || !cutGroup.containsAll(Arrays.asList("Анастасия", "Юрий"))
+                || cutGroup.contains("Ксения")) {
+            throw new AssertionError("Ответ cutGroup содержит неправильный список имен");
+        }
+
+        double sum = UtilClass.sum(numbers1, numbers2);
+
+        if (sum != 13.5) {
+            throw new AssertionError("Ответ должен быть 13.5");
+        }
+
+        System.out.print("Поздравляю! Задание выполнено");
+    }
 }
--- src/practicesb/day5/UtilClass.java
+++ src/practicesb/day5/UtilClass.java
@@ -1,4 +1,123 @@
 package practicesb.day5;
 
-public class UtilClass {
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Сделайте методы типизированными (Используйте generics)
+ */
+public class UtilClass<E> {
+    /**
+     * Возвращает true, если список равен null или если список пуст
+     *
+     * @param list список.
+     * @return true, если список равен null или если список пуст
+     */
+    public static <E> boolean isBlank(List<E> list) {
+        return list == null || list.isEmpty();
+    }
+
+    /**
+     * Получает последний элемент в коллекции
+     * Если список равен null, то выбрасывается исключение IllegalArgumentException
+     *
+     * @param list список
+     * @return последний элемент в коллекции
+     */
+    public static <E> E getLastElem(List<E> list) {
+        if (list == null) {
+            throw new IllegalArgumentException();
+        }
+        return list.get(list.size() - 1);
+    }
+
+    /**
+     * Получает элемент из списка.
+     * Если в аргументах список или элемент равен null, то выбрасывается исключение IllegalArgumentException
+     * Если элемент не найден, то возвращаем null
+     *
+     * @param list список, в котором ищем элемент
+     * @param elem элемент, который ищется в списке.
+     * @return элемент из списка.
+     */
+    public static <E> Object findElem(List<E> list, E elem) {
+        if (list == null || elem == null) {
+            throw new IllegalArgumentException();
+        }
+        for (E temp : list) {
+            if (temp == elem) {
+                return temp;
+            }
+        }
+        return null;
+    }
+
+    /**
+     * Объединяет два списка.
+     * Если хотя-бы один из аргументов равен null, то выбрасываем исключение IllegalArgumentException
+     * Аргументы метода не меняются
+     *
+     * @param list1 первый список
+     * @param list2 второй список
+     * @return возвращает объединенный список.
+     */
+    public static <E> List<E> union(List<E> list1, List<E> list2) {
+        if (list1 == null || list2 == null) {
+            throw new IllegalArgumentException();
+        }
+        List<E> res = new ArrayList<>();
+        res.addAll(list1);
+        res.addAll(list2);
+        return res;
+    }
+
+    /**
+     * Удаляет все элементы из списка list1 всех элементов, которые встречаются в списке list2
+     * Если хотя-бы один из аргументов равен null, то выбрасываем исключение IllegalArgumentException
+     * Аргументы метода не меняются
+     *
+     * @param list1 первый список
+     * @param list2 второй список
+     * @return возвращает список, который содержит элементы из списка list1, в котором удалены все элементы из
+     * списка list2.
+     */
+    public static <E> List<E> removeAll(List<E> list1, List<E> list2) {
+        if (list1 == null || list2 == null) {
+            throw new IllegalArgumentException();
+        }
+        List<E> res = new ArrayList<>();
+        for (E temp : list1) {
+            if (!(list2.contains(temp))) {
+                res.add(temp);
+            }
+        }
+        return res;
+    }
+
+    /**
+     * Складывает числа из обоих списков и возвращает сумму.
+     * Если хотя-бы один из аргументов равен null, то выбрасываем исключение IllegalArgumentException
+     * Аргументы метода не меняются
+     * Подсказка:
+     * 1) Помните, что все объекты чисел (Integer, Double, Float и т.д) наследуются от общего предка Number.
+     * 2) Используйте ограничения для типизации, чтобы списки могли содержать любые числа (Integer, Double, Float и т.д)
+     * 3) Для приведения Number к double воспользуйтесь num.doubleValue();
+     *
+     * @param list1 первый список
+     * @param list2 второй список
+     * @return сумма чисел.
+     */
+    public static <E> double sum(List<? extends Number> list1, List<? extends Number> list2) {
+        if (list1 == null || list2 == null) {
+            throw new IllegalArgumentException();
+        }
+        double res = 0;
+        for (Number num : list1) {
+            res += num.doubleValue();
+        }
+        for (Number num : list2) {
+            res += num.doubleValue();
+        }
+        return res;
+    }
 }
